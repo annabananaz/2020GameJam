cc5e61d884a9687d95dc560835933da0034851b6
Lewis Truong <letruong@csumb.edu>
2020-04-18 12:53:08 -0700

    Initial Build of Egg Game

---
+++ Project Egg/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float maxPitch;                      //The maximum pitch to go down
+    public float minPitch;                      //The minimum pitch to go up
+    public float cameraSpeed;                   //The speed of the camera movement
+    //public Transform holdingPosTransform;       //The transform of the object in player's hands
+    public bool lockCursor = true;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    private float isPausedSpeed;
+    private bool m_cursorIsLocked = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // USE WHEN PAUSE MENU IMPLEMENTED
+        /*if (PauseMenuScript.gameIsPaused)
+        {
+            isPausedSpeed = 0.0f;
+        }
+        else
+        {
+            isPausedSpeed = 1.0f;
+        }*/
+
+        // REMOVE WHEN PAUSE MENU IMPLEMENTED 
+        isPausedSpeed = 1.0f;
+
+        yaw += cameraSpeed * isPausedSpeed * Input.GetAxis("Mouse X");                                  //Moves from left and right
+        pitch -= cameraSpeed * isPausedSpeed * Input.GetAxis("Mouse Y");                                //Moves from up and down
+        pitch = Mathf.Clamp(pitch, minPitch, maxPitch);                                 //Gives limits/parameters to prevent revolving 
+
+        //holdingPosTransform.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);      //Turns the object in hand from up, down, left and right
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);                          //Turns the transform of where this script is located from up, down, left, and right
+
+        UpdateCursorLock();
+    }
+
+    public void SetCursorLock(bool value)
+    {
+        lockCursor = value;
+        if (!lockCursor)
+        {//we force unlock the cursor if the user disable the cursor locking helper
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void UpdateCursorLock()
+    {
+        //if the user set "lockCursor" we check & properly lock the cursos
+        if (lockCursor)
+        {
+            InternalLockUpdate();
+        }
+    }
+
+    private void InternalLockUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            m_cursorIsLocked = false;
+        }
+        else if (Input.GetMouseButtonUp(0)) //&& !PauseMenuScript.gameIsPaused) READD WHEN PAUSE MENU IS IMPLMENTED
+        {
+            m_cursorIsLocked = true;
+        }
+
+        if (m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else if (!m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
---
+++ Project Egg/Assets/Scripts/PlayerSight.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSight : MonoBehaviour
+{
+    public GameObject FirstPersonCamera;
+    float sightDistance;                        //Raycast length
+    public static bool isHolding;               //Checks if the player is already holding an object
+    public Transform playerHoldingPosition;     //Transform where the object will be held
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirstPersonCamera = GameObject.Find("Main Camera").gameObject;
+        playerHoldingPosition = GameObject.Find("HoldingPosition").transform;
+        sightDistance = 4.5f;
+        isHolding = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 forward = FirstPersonCamera.transform.TransformDirection(Vector3.forward);                  //Uses the FirstPersonCamera as the Ray
+        RaycastHit hit;                                                                                     //Raycast hit
+        Debug.DrawRay(FirstPersonCamera.transform.position, forward * sightDistance, Color.yellow);         //Debug draws the raycast lines
+
+        if (Physics.Raycast(FirstPersonCamera.transform.position, forward, out hit, sightDistance))
+        {
+            float rayDistance = hit.distance;
+            if (rayDistance <= sightDistance)
+            {
+                if (hit.transform.tag != "Player")
+                {
+                    //Pick Up physics Object
+                    if (hit.transform.gameObject.GetComponent<Rigidbody>() != null)
+                    {
+                        //Physically Carry the Object
+                        if (!isHolding)
+                        {
+                            //If wanting to pick up & move object
+                            if (Input.GetKeyDown(KeyCode.Mouse0))
+                            {
+                                CarryObject(hit.transform.gameObject);
+                            }
+                        }
+                        //Physically Drop the Object
+                        else if (isHolding && (playerHoldingPosition.childCount > 0))
+                        {
+                            //if wanting to release object
+                            if (Input.GetKeyUp(KeyCode.Mouse0))
+                            {
+                                //print("Right released");
+                                DropObject(hit.transform.gameObject);
+                            }
+
+                            if (Input.GetKey(KeyCode.Mouse1))
+                            {
+
+                                ThrowObject(hit.transform.gameObject, hit);
+                            }
+                        }
+                    }
+                    // Press button
+                    if (hit.collider.tag.Equals("Button"))
+                    {
+                        // Buttons for obstacles
+                    }
+                }
+            }
+        }
+    }
+
+    //==========================
+    //CARRY OBJECT
+    //==========================
+    private void CarryObject(GameObject hitObject)
+    {
+
+        if (hitObject.transform.parent != null && !(hitObject.transform.parent.name.Equals(playerHoldingPosition.name)) && !(hitObject.transform.parent.tag.Equals("Untagged")))
+        {
+            hitObject = hitObject.transform.parent.gameObject;
+        }
+
+        hitObject.GetComponent<Rigidbody>().isKinematic = true;
+        hitObject.GetComponent<Rigidbody>().useGravity = false;
+        hitObject.transform.position = playerHoldingPosition.position;
+        hitObject.transform.parent = GameObject.Find("HoldingPosition").transform;
+        sightDistance = 3.5f;
+        isHolding = true;
+    }
+
+    //==========================
+    //DROP OBJECT
+    //==========================
+    private void DropObject(GameObject hitObject)
+    {
+        if (hitObject.transform.parent != null && !(hitObject.transform.parent.name.Equals(playerHoldingPosition.name)))
+        {
+            hitObject = hitObject.transform.parent.gameObject;
+        }
+
+        hitObject.GetComponent<Rigidbody>().isKinematic = false;
+        hitObject.GetComponent<Rigidbody>().useGravity = true;
+        hitObject.transform.parent = null;
+        sightDistance = 7.0f;
+        isHolding = false;
+    }
+
+    //==========================
+    //THROW OBJECT
+    //==========================
+    private void ThrowObject(GameObject hitObject, RaycastHit hit)
+    {
+        if (hitObject.transform.parent != null && (!(hitObject.transform.parent.name.Equals(playerHoldingPosition.name)) && !(hitObject.transform.parent.name.Equals("GravPos"))))
+        {
+            hitObject = hitObject.transform.parent.gameObject;
+        }
+
+        hitObject.GetComponent<Rigidbody>().isKinematic = false;
+        hitObject.GetComponent<Rigidbody>().useGravity = true;
+        hitObject.transform.parent = null;
+        hit.rigidbody.AddForce(-hit.normal * 600);
+        isHolding = false;
+    }
+}
